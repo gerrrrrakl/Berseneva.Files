919b1f704f168b90a477e4a5e0288f63efbcecc2
DESKTOP-LHD7A0P\ACER <gerrrrrakl>
2022-04-08 11:42:41 +0300

    Добавьте файлы проекта.

---
+++ Berseneva.Files.Exercise2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Text.Encodings;
+using System.Text.Encodings.Web;
+using System.IO;
+using System.Text.Json;
+
+
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = @"D:\Учеба ИАТЭ\Berseneva.Files\Berseneva.Files.Exercise2\Table.csv";
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.GetEncoding(1251);
+
+
+            var lines = File.ReadAllLines(path, encoding);
+            var persons = new Person[lines.Length - 1];
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var splits = lines[i].Split(';');
+                var person = new Person();
+                person.Номер = Convert.ToDouble(splits[0]);
+                person.Фамилия = splits[1];
+                person.Оплата_в_час = Convert.ToDouble(splits[2]);
+                person.Отработанное_время = Convert.ToDouble(splits[3]);
+                persons[i - 1] = person;
+                Console.WriteLine(person);
+            }
+
+            var result = "result.csv";
+            using StreamWriter streamWriter = new StreamWriter(result, false, encoding); 
+            {
+                streamWriter.WriteLine($"Фамилия;Номер;Оплата_в_час;Отработанное_время;Зарплата");
+                for (int i = 0; i < persons.Length; i++)
+                {
+                    string stroka = persons[i].ToExcel();
+                    streamWriter.WriteLine(stroka);
+                }
+            }
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Default
+            };
+
+            var json = JsonSerializer.Serialize(persons, jsonOptions);
+            File.WriteAllText("result.json", json);
+
+            var stringJson = File.ReadAllText("result.json");
+            var array = JsonSerializer.Deserialize<Person[]>(stringJson);
+        }
+    }
+    public class Person
+    {
+        public double Номер { get; set; }
+        public string Фамилия { get; set; }
+        public double Оплата_в_час { get; set; }
+        public double Отработанное_время { get; set; }
+
+
+
+        public double Зарплата { get => Оплата_в_час * Отработанное_время; }
+
+        public override string ToString()
+        {
+            return $"Номер: {Номер} Фамилия: {Фамилия}  Оплата в час: {Оплата_в_час}  Отработанное время:{Отработанное_время}  Зарплата:{Зарплата}";
+        }
+
+        public string ToExcel()
+        {
+            return $"{Номер};{Фамилия};{Оплата_в_час};{Отработанное_время};{Зарплата}";
+        }
+
+    }
+}
---
+++ Berseneva.Files/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Абсолютный путь.
+            string path = @"D:\Учеба ИАТЭ\Berseneva.Files\Berseneva.Files\TextFile1.txt";
+            // Относительный путь.
+            string pathw = @"..\..\..\TextFile1.txt";
+            FileInfo info = new FileInfo(path); 
+            string Text = File.ReadAllText(path);
+            string p = "";
+            string[] textMass;
+            StreamReader jk = new StreamReader(path);
+            while (jk.EndOfStream != true)
+            {
+                p += jk.ReadLine();
+            }
+            textMass = p.Split(' ');
+            jk.Close();
+
+            var result = "result.txt";
+
+            using (StreamWriter streamWriter = new StreamWriter(result))
+            {
+                streamWriter.WriteLine("Название файла " + info.Name);
+                streamWriter.WriteLine("Абсолютный путь: " + info.FullName);
+                streamWriter.WriteLine("Относительный путь: " + pathw);
+                streamWriter.WriteLine("Время создания: " + info.CreationTime);
+                streamWriter.WriteLine("Размер файла: " + info.Length);
+                streamWriter.WriteLine("Общее количество слов: " + textMass.Length);
+                streamWriter.WriteLine("Общее количество строк: " + File.ReadAllLines(path).Length);
+                streamWriter.WriteLine("Присутствуют ли в тексте цифры: " + Regex.IsMatch(Text, @"[0-9]"));
+                streamWriter.WriteLine("Eсть ли в тексте кириллица? " + Regex.IsMatch(Text, @"[А-я]"));
+                streamWriter.WriteLine("Eсть ли в тексте латиница? " + Regex.IsMatch(Text, @"[A-z]"));
+            }
+
+        }
+
+    }
+}
